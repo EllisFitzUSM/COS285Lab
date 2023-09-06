8227b5f44d74ec0f691b264053fce5dda18f7ffc
ellis <ellis@localhost>
2023-09-06 17:52:30 -0400

    Added top file documentation.

--- Fraction.java
+++ Fraction.java
@@ -1,4 +1,9 @@
+/**
+ * Fraction - A class that encapsulates a fraction value by seperating the numerator and denominator
+ * @author Ellis, Josh, Jonah, Gabrielle
+ * @version 6 September 2023
+ */
 public class Fraction implements INumber {
-    public int numerator;
-    public int denominator;
+    int numerator;
+    int denominator;
 }
