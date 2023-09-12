81fe956cffbd69e48feaac65de3931d839632822
SuperCudder <144271447+SuperCudder@users.noreply.github.com>
2023-09-12 15:02:40 -0400

    Method implemented: reverseOrder

---
+++ src/Fraction.java
@@ -0,0 +1,9 @@
+/**
+ * Fraction - A class that encapsulates a fraction value by seperating the numerator and denominator
+ * @author Ellis, Josh, Jonah, Gabrielle
+ * @version 6 September 2023
+ */
+public class Fraction implements INumber {
+    int numerator;
+    int denominator;
+}
